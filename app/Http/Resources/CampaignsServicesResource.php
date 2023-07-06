632997baa6b3e40006de3240c142addab730b457
Mohamad Risha <momomohamad0@gmail.com>
2023-07-06 14:47:05 +0300

    Pivot tables Relations&Resource edits

--- app/Http/Resources/CampaignsServicesResource.php
+++ app/Http/Resources/CampaignsServicesResource.php
@@ -14,15 +14,42 @@ class CampaignsServicesResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        // return [
+        //     'id' => $this->id,
+        //     'amount' => $this->amount,
+        //     'description' => $this->description,
+        //     'start_date' => $this->start_date,
+        //     'end_date' => $this->end_date,
+        //     'status' => $this->status,
+        //     'service_id' => $this->service_id,
+        //     'campaign_id' => $this->campaign_id,
+        // ];
+
+        $data = [
             'id' => $this->id,
             'amount' => $this->amount,
             'description' => $this->description,
             'start_date' => $this->start_date,
             'end_date' => $this->end_date,
             'status' => $this->status,
-            'service_id' => $this->service_id,
-            'campaign_id' => $this->campaign_id,
+            // 'service_id' => $this->service_id,
+            // 'campaign_id' => $this->campaign_id,
         ];
+
+        if (!is_null($this->campaign)) {
+            $data['campaign_id'] = $this->campaign->id;
+            $data['campaign_title'] = $this->campaign->title;
+        } else {
+            $data['campaign'] = $this->campaign; //null for sure
+        }
+
+        if (!is_null($this->service)) {
+            $data['service_id'] = $this->service->id;
+            $data['service_name'] = $this->service->name;
+        } else {
+            $data['service'] = $this->service; //null for sure
+        }
+
+        return $data;
     }
 }
--- app/Models/CampaignsBeneficiaries.php
+++ app/Models/CampaignsBeneficiaries.php
@@ -10,11 +10,17 @@ class CampaignsBeneficiaries extends Model
 {
     use HasFactory, SoftDeletes;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    public function campaign()
+    {
+        return $this->belongsTo(Campaign::class, 'campaign_id', 'id');
+    }
+
+    public function beneficiary()
+    {
+        return $this->belongsTo(Beneficiary::class, 'beneficiary_id', 'id');
+    }
+
+
     protected $hidden = [
         'created_at',
         'updated_at',

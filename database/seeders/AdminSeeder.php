b5d16b5fc9a2d65785954ce0cfa46117a1f9e9f8
Moamen Siam <moaman@gmail.com>
2023-07-10 20:24:28 +0300

    seed

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -13,7 +14,11 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::factory(10)->create();
-
+       // Admin::factory(10)->create();
+        Admin::create([
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make(12345)
+        ]); 
     }
 }
